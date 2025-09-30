18bbe06a1eb9ed0d1895f283ab709d3a7011a971
cc9548 <hhh572531@gmail.com>
2025-10-01 03:57:20 +0800

    2

---
+++ 1toN.js
@@ -0,0 +1,8 @@
+function sun(n){
+    var result=0;
+    var i=0;
+    while(i<n){
+        result +=i;
+    }
+    return result;
+}
--- avg.js
+++ avg.js
@@ -1,13 +1,23 @@
+// function avg(score){
+//    var sum=0, average=0;
+//    for(var i=0; i<score.length; i++){
+//        sum+=score[i];
+//    }
+//    if(score.length>0)
+//        average=sum/score.length;
+//    return average;
+//    
+//}
 function avg(score){
     var sum=0, average=0;
-    for(var i=1; i<score.length; i++){
+    for(var i=0; i<score.length; i++){
         sum+=score[i];
     }
     if(score.length>0)
         average=sum/score.length;
-        return average;
+    return average;
     
 }
 //test
 var Score=[10,20,30,55,45,60];
-console. log(Score .toString="average is "+avg(Score));
+console.log(Score .toString="average is "+avg(Score));
