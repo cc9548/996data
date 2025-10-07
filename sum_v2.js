644fdd1dae1f2fa5cd9a220ef155e7ecd72c41a5
cc9548 <hhh572531@gmail.com>
2025-10-07 14:36:57 +0800

    2

--- sum_v2.js
+++ sum_v2.js
@@ -1,20 +1,20 @@
 //calc 1-2+3-4+...n
+const readline = require("readline-sync");
 function sum2(n){
-    var sum=0;
-    if(n<0){
+    let sum=0;
+    if(n<=0){
         console.log("error: n must >0")
         return 696;
     }
     else {
-        for(var i=1;i<=n;i++){
-            if(i%2==0)
-                sum -= i;//even
-                else
-                sum += i;//odd
+        for(let i=1;i<=n;i++){
+            if(i%2===0) sum-=i;
+            else sum+=i ;              
             }
         return sum;
-        }
+    }
 }
 //test 
-var n=100;
-console.log ("1-2+3-4+..."+n+"="+sum2(n));
+//readline
+let n=readline.questionInt("Enter a number:");
+console.log ("1-2+3-4+...+n="+sum2(n));
