823f72adde20d4609a0d5fa8da156ab96d148990
cc9548 <hhh572531@gmail.com>
2025-10-07 15:45:09 +0800

    2025100702

--- factorial.js
+++ factorial.js
@@ -4,6 +4,13 @@ function fact(n){
     else
     return n*fact(n-1);
 }
+// recursive sum 1+2+3+...+n
+function sum(n){
+    if(n==1)
+        return 1;
+    else 
+    return n+sum(n-1);
+}
 //test
 let n=3;
 console.log(n+"!="+fact(n));
